d4c6182f8b100053d377bb4b80d29c7b36dadbad
ThiagoAgo <116441661+ThiagoAgo@users.noreply.github.com>
2022-10-27 17:36:39 -0300

    O tempo agora pode ser alterado
    Adicionada opção de alterar o tempo de cada estado do sinal via comunicação Serial

--- Semáforo_para_pedestres-Arduino-Uno-.ino
+++ Semáforo_para_pedestres-Arduino-Uno-.ino
@@ -3,18 +3,145 @@
 #define Sbit(N)    (1<<N)
 #define Rstbit(N) ~(1<<N)
 volatile byte Pedido_de_passagem = 0;
-volatile byte contador = 0; // CONTA NUMERO DE INTERUPCOES DE 2,5 segundos
+volatile byte contador = 0; // CONTA NUMERO DE INTERUPCOES DE 1 segundo
+volatile byte tempoVerde = 1;
+volatile byte tempoAmarelo = 1;
+volatile byte tempoVermelho = 1;
 
-enum Estd {Verde, Amarelo, Vermelho, Vermelho_Fim} semafaro = Verde; 
+// definindo os estados
+enum {INI, LAMPADA, TEMPO1, TEMPO2,FIM} estMQ;
+enum Estd {Verde, Amarelo, Vermelho} semafaro = Verde; 
+
+void configurarTempo(byte T1, byte T2 , byte c){
+    switch(c){
+      case 1:
+          // Subtraimdo o valor de zero da tabela ASCII para obter o valor numerico referente ao char
+          tempoVerde = ((T2-48)+(10*(T1-48)));  
+          break;
+        case 2:
+          tempoAmarelo = ((T2-48)+(10*(T1-48)));    
+          break;
+        case 3:
+          tempoVermelho = ((T2-48)+(10*(T1-48)));   
+          break;
+    }
+}
+
+// armazenamento temporario do valor e esperar 
+char valorT1, valorT2 =0;
+byte cor = 0; // verde = 1, amarelo = 2 e vermelho = 3
+void processaMQ(char m){
+  switch(estMQ){
+    case INI:
+      if(m=='I'){
+      estMQ = LAMPADA;
+      }break;
+    
+    case LAMPADA: // estado para Esperar mensagem iniciar
+      switch(m){
+          case 'A':
+              estMQ = TEMPO1;
+            cor = 1;
+              break;
+            case 'B':
+              estMQ = TEMPO1;
+            cor = 2;
+              break;
+      case 'C':
+              estMQ = TEMPO1;
+            cor = 3;
+              break;
+            default:
+              Serial.println("Byte de Cor nao conhecido as cores sao:");
+              Serial.println("Verde = A, Amarelo = B e Vermelho = C");
+                estMQ = INI;
+              break;
+        }
+    break;
+    
+    case TEMPO1:  // tratar a mensagem e verificar se e valida
+    if(m>='0' && m<='9'){
+      valorT1=m;
+      estMQ=TEMPO2;
+    }
+    else{
+      estMQ=INI;
+      Serial.println("Tempo1 invalido escreva um valor de 0 a 9");
+    }
+    break;
+    
+    case TEMPO2:  // tratar a mensagem e verificar se e valida
+    if(m>='0' && m<='9'){
+      valorT2=m;
+      estMQ=FIM;
+    }
+    else{
+      estMQ=INI;
+      Serial.println("Tempo2 invalido escreva um valor de 1 a 9");
+    }
+    break;
+    
+    case FIM:
+    if(m=='F'){ // efetivamente executar o processamento
+     configurarTempo(valorT1, valorT2, cor);
+     Serial.println("Os dados foram salvos");}
+    else{
+    Serial.println("BYTE de filalizaçao incorreto, nada foi alterado.");
+    }
+    estMQ=INI;
+    break;
+  }
+}
+
+
+void LuzesSemafaro(){
+    switch (semafaro){
+        case (Verde):
+        PORTC |= (Sbit(PORTC2));
+        PORTC &= ~(Sbit(PORTC0)|Sbit(PORTC1));
+        if(contador >= tempoVerde &&   Pedido_de_passagem == 1){
+          contador = 0;
+            Pedido_de_passagem = 0;
+            semafaro = Amarelo;}
+      break;
+      
+      case (Amarelo):
+        PORTC |= (Sbit(PORTC1));
+        PORTC &= ~(Sbit(PORTC0)|Sbit(PORTC2));
+        if (contador >= tempoAmarelo) {
+          contador = 0;
+            semafaro = Vermelho;}
+        break;
+      
+      case (Vermelho):
+        PORTC |= (Sbit(PORTC0));
+        PORTC &= ~(Sbit(PORTC1)|Sbit(PORTC2));
+        if (contador >= tempoVermelho) {
+          contador =0;
+            semafaro = Verde;}
+        break;
+        
+      default :
+      semafaro = Verde;
+    }
+} 
+
+void serialEvent(){
+  char ch;
+  while(Serial.available()){
+    ch=Serial.read(); 
+    processaMQ(ch);
+  }
+}
 
 void configuraPinos(){
-  	// pinos com saidas e iniciam em zero
-	DDRC |= (Sbit(DDC0)|Sbit(DDC1)|Sbit(DDC2)|Sbit(DDC3)|Sbit(DDC4));
-    PORTC &= ~(Sbit(PORTC0)|Sbit(PORTC1)|Sbit(PORTC2)|Sbit(PORTC3)|Sbit(PORTC4));
-  	// pino 2 como entrada pull-up
-  	MCUCR &= Rstbit(PUD); 
-  	PORTD |= Sbit(PORTD2);
-  	DDRD &= Rstbit(DDD2);  	
+    // pinos com saidas e iniciam em zero
+  DDRC |= (Sbit(DDC0)|Sbit(DDC1)|Sbit(DDC2));
+    PORTC &= ~(Sbit(PORTC0)|Sbit(PORTC1)|Sbit(PORTC2));
+    // pino 2 como entrada pull-up
+    MCUCR &= Rstbit(PUD); 
+    PORTD |= Sbit(PORTD2);
+    DDRD &= Rstbit(DDD2);   
 }
 
 void configurar_ISR(){
@@ -33,69 +160,28 @@ ISR(INT0_vect){
 
 void configurarTimer1(){
   // configurar o timer com CTC
-	TCCR1A &= ~(Sbit(WGM11)|Sbit(WGM10));
-  	TCCR1B |= Sbit(WGM12);
-  	TCCR1B &= ~(Sbit(WGM13));
+  TCCR1A &= ~(Sbit(WGM11)|Sbit(WGM10));
+    TCCR1B |= Sbit(WGM12);
+    TCCR1B &= ~(Sbit(WGM13));
   // escolhe prescaler como 1024
-  	TCCR1B |= (Sbit(CS12)|Sbit(CS10));
-  	TCCR1B &= ~(Sbit(CS11));
-  	// Configurar  valor maximo da contagem
-  	OCR1A=39062;  
-  	TCNT1 = 0; // zerar a contagem
-  	TIMSK1 |= Sbit(OCIE1A); // habilita a interupcao do timer 1 
-	//resutado final gera interucao a cada 2,5 segundos
+    TCCR1B |= (Sbit(CS12)|Sbit(CS10));
+    TCCR1B &= ~(Sbit(CS11));
+    // Configurar  valor maximo da contagem
+    OCR1A=15625;  
+    TCNT1 = 0; // zerar a contagem
+    TIMSK1 |= Sbit(OCIE1A); // habilita a interupcao do timer 1 
+  //resutado final gera interucao a cada 1 segundo
 }
 
 ISR(TIMER1_COMPA_vect){
   contador++;
 }
 
-void LuzesSemafaro(){
-    switch (semafaro){
-      	case (Verde):
-    		PORTC |= (Sbit(PORTC2)|Sbit(PORTC3));
-    		PORTC &= ~(Sbit(PORTC0)|Sbit(PORTC1)|Sbit(PORTC4));
-      	if(contador == 4 &&   Pedido_de_passagem == 1){
-      		contador = 0;
-            Pedido_de_passagem = 0;
-          	semafaro = Amarelo;}
-    	break;
-      
-  		case (Amarelo):
-    		PORTC |= (Sbit(PORTC1)|Sbit(PORTC3));
-    		PORTC &= ~(Sbit(PORTC0)|Sbit(PORTC2)|Sbit(PORTC4));
-      	if (contador == 2) {
-        	contador = 0;
-          	semafaro = Vermelho;}
-      	break;
-      
-  		case (Vermelho):
-    		PORTC |= (Sbit(PORTC0)|Sbit(PORTC4));
-    		PORTC &= ~(Sbit(PORTC1)|Sbit(PORTC2)|Sbit(PORTC3));
-      	if (contador == 12) {
-        	contador =0;
-          	semafaro = Vermelho_Fim;}
-      	break;
-      	
-      	case (Vermelho_Fim):
-      		PORTC |= (Sbit(PORTC0)|Sbit(PORTC4));
-    		PORTC &= ~(Sbit(PORTC1)|Sbit(PORTC2));
-      		PORTC ^= (1<<PORTC3);
-      	if (contador ==4){
-      		contador =0;
-          	semafaro = Verde;
-        }
-      	break;
-
-    	default :
-    	semafaro = Verde;
-    }
-} 
-
 void setup(){
-  	configuraPinos();
-  	configurar_ISR();
-  	configurarTimer1();	
+    Serial.begin(9600);
+    configuraPinos();
+    configurar_ISR();
+    configurarTimer1(); 
 }
 
 void loop(){
